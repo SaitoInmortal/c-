24e6698cc91f7dc623f4a1e796d41072eec3a89f
SaitoInmortal <114177249+SaitoInmortal@users.noreply.github.com>
2023-04-19 01:07:37 -0500

    ejercicio 1

---
+++ Program1.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese su nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine("tu nombre es " + nombre);
