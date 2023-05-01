49872392faf570b3de6253fc3dbfcd1510d68abd
SaitoInmortal <114177249+SaitoInmortal@users.noreply.github.com>
2023-04-30 21:12:45 -0500

    Update ejemplo1.cs

--- dia6/ejemplo1.cs
+++ dia6/ejemplo1.cs
@@ -1,1 +1,17 @@
-
+// See https://aka.ms/new-console-template for more information
+float p = 20f;
+Console.WriteLine("Ingrese su nombre:");
+string n = Console.ReadLine();
+Console.WriteLine("Ingrese su correo:");
+string e = Console.ReadLine();
+Console.WriteLine("Tiene un cupon?:");
+string d = Console.ReadLine();
+d = d.ToUpper();
+if (d == "SI" || d == "Y")
+{
+    float r = p - 5f;
+    Console.WriteLine("Total a pagar es: "+ r);
+}else
+{
+    Console.WriteLine("Total a pagar es: " + p);
+}
